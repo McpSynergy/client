f9603879e38bd0392e07d6c2a7f18bf3d1990105
YongGit <1013588891@qq.com>
2025-04-23 20:15:57 +0800

    style: react app

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -2,6 +2,7 @@ import { DynamicRender } from "@mcp-synergy/react";
 function App() {
   return (
     <div>
+      {/* 插件在做解析的时候，这里的路径需要处理成项目的绝对路径？ */}
       <DynamicRender path={`${process.env.ROOT_DIR}/src/DyApp.tsx`} />
     </div>
   );
